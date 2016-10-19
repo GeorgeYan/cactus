44e494c1383f32f568e9a9b15cbaecf64adde8d3
闫毅思 <yanyisi@xdstar.net>
2016-10-19 15:39:57 +0800

    use database

--- priv/repo/migrations/20161018061637_create_user.exs
+++ priv/repo/migrations/20161018061637_create_user.exs
@@ -6,6 +6,8 @@ defmodule Rumbl.Repo.Migrations.CreateUser do
       add :name, :string
       add :username, :string, null: false
       add :password_hash, :string
+
+      timestamps
     end
 
     create unique_index(:users, [:username])
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,8 +1,9 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  alias Rumbl.User
 
   def index(conn, _params) do
-    users = Repo.all(Rubml.User)
+    users = Repo.all(User)
     #users =
     #[
       #%Rumbl.User{id: "1", name: "George", username: "yan", password: "elixir"},
@@ -13,7 +14,12 @@ defmodule Rumbl.UserController do
   end
 
   def show(conn, %{"id" => id }) do
-    user = Repo.get(Rumbl.User, id)
+    user = Repo.get(User, id)
     render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 end
