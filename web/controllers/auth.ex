b9aaef7bb93da0b9b6e6445d8f131ad708859f49
GeorgeYan <abcyys123@gmail.com>
2016-11-18 00:39:04 +0800

    channel 80%

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -3,6 +3,7 @@ defmodule Rumbl.Auth do
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
   import Phoenix.Controller
   alias Rumbl.Router.Helpers
+  require Logger
 
   def init(opts) do
     Keyword.fetch!(opts, :repo)
@@ -10,12 +11,10 @@ defmodule Rumbl.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    #user = user_id && repo.get(Rumbl.User, user_id)
-    #assign(conn, :current_user, user)
+
     cond do
       user = conn.assigns[:current_user] -> put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) -> put_current_user(conn, user)
-
       true ->
         assign(conn, :current_user, nil)
     end
@@ -23,7 +22,7 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -60,7 +59,7 @@ defmodule Rumbl.Auth do
   defp put_current_user(conn, user) do
     token = Phoenix.Token.sign(conn, "user socket", user.id)
     conn
-    |> assign(:currenter, user)
+    |> assign(:current_user, user)
     |> assign(:user_token, token)
   end
 end
